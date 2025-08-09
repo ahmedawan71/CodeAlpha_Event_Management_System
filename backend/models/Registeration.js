3c2b395e0ea9a4730f1ce79a36aae559601a7d91
Muhammed Ahmed <m.ahmedse05@gmail.com>
2025-08-09 22:59:02 +0500

    Create models for User, Event, and Registration

---
+++ backend/models/Registeration.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const RegistrationSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+});
+
+module.exports = mongoose.model('Registration', RegistrationSchema);
